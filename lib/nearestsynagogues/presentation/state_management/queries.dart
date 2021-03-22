c7a1d7111fff7d6deba01adf403ef89291c8662a
MatanLazimi <matanlazimi@gmail.com>
2021-03-22 22:55:43 +0200

    query nearby update and geolocator

--- lib/nearestsynagogues/presentation/state_management/queries.dart
+++ lib/nearestsynagogues/presentation/state_management/queries.dart
@@ -2,24 +2,34 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:geolocator/geolocator.dart';
 
 Future<void> getSynagogues(Position pos) async {
-  var x = pos.latitude.toDouble() + 0.00500;
   int count = 1;
-  print(
-      "sfajkhdsfkjdgsjkgdfjkgdfjkdfgkjdfgkjdfgkjdfgkjdgfjkdfgkjdgfjkdgfjkdgfjkdgfjkfgjkfgdjkdfgkjjkjgkdgfjkdgfkj");
-  print(x);
   FirebaseFirestore.instance
       .collection('Synagogues')
       .where('lat', isLessThanOrEqualTo: '${pos.latitude.toDouble() + 0.00500}')
       .where('lat',
           isGreaterThanOrEqualTo: '${pos.latitude.toDouble() - 0.00500}')
-      // .where('lon',
-      //     isLessThanOrEqualTo: '${pos.longitude.toDouble() + 0.00500}')
-      // .where('lon',
-      //     isGreaterThanOrEqualTo: '${pos.longitude.toDouble() - 0.00500}')
-      .snapshots();
-  // .listen((data) => data.docs.forEach((doc) =>
-  //     print(doc["name"] + " " + doc["lat"] + 'count = ${count++}')));
+      .snapshots()
+      .listen((data) => data.docs.forEach((doc) => isLessThen500m(
+                  pos.latitude.toDouble(),
+                  pos.longitude.toDouble(),
+                  double.parse(doc["lat"]),
+                  double.parse(doc["lon"])) ==
+              true
+          ? print(doc["name"] + " " + 'count = ${count++}')
+          : null));
 }
+
+bool isLessThen500m(double ourLatitude, double ourLongitude,
+    double destLatitude, double destLongitude) {
+  double distanceInMeters = Geolocator.distanceBetween(
+      ourLatitude, ourLongitude, destLatitude, destLongitude);
+  if (distanceInMeters <= 500 && distanceInMeters >= 0)
+    return true;
+  else
+    return false;
+}
+
+
   /*
   Example for insert data to variable:
    your_async_method () async {
