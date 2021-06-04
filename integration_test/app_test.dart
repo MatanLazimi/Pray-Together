7c4837ea702d201d971ca2a8466a795a00a471f3
Elhanan <bsnani7@gmail.com>
2021-06-04 13:45:20 +0300

    continue tests - only login test

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_signin_button/button_view.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
+import 'package:pray_together/login/presentation/pages/login_page.dart';
 // TODO: Import the app that you want to test
 import 'package:pray_together/main.dart' as app;
 
@@ -13,8 +15,10 @@ void main() {
       // TODO: wait until the app has settled - settled mean that the frame from before does not changed anything
       await tester.pumpAndSettle();
       //TODO: create finders for email. password and login
-      final emailFormField = find.byType(ElevatedButton).first;
+      final loginbutton = find.byType(SignInButton).first;
       // TODO: Tap on the login button and wait till it settled
+      await tester.tap(loginbutton);
+      await tester.pumpAndSettle();
     });
   });
 }
